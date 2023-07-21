56194a001473549741046cbd2ad4624ababf7c2a
KirillMaster <suhorukih@mail.ru>
2023-07-21 19:48:32 +0600

    init

---
+++ AES_tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AES_tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
